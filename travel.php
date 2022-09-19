2ea44491fdbc5a2c19d56081b72d146be593d7a9
Sittichai <sittichai.sa@rmutsvmail.com>
2022-09-19 15:53:40 +0700

    Create travel.php

---
+++ travel.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include "includes/header.php"; ?>
+
+  <head>
+
+  <body>
+    <?php include "includes/navbar.php"; ?>
+
+    <div class="container">
+      <!-- start -->
+      <h2>ท่องเที่ยว</h2>
+      <hr>
+      <!-- end  -->
+    </div>
+
+  </body>
+
+</html>
+<?php include "includes/footer.php"; ?>
